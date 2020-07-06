f1c79ebcb2061b3c932022aad494e2eb204f6d84
mastergammler <raphael-hoppe@gmx.de>
2020-07-06 12:20:32 +0200

    Removed the editor tooling from the progress bar script

--- Editor/EditorExtensions.cs
+++ Editor/EditorExtensions.cs
@@ -1,31 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-#if UNITY_EDITOR
 using UnityEditor;
-#endif
 
-namespace MgSq.UI
+namespace MgSq.UI.Editor
 {
     public class EditorExtensions
     {
-        #if UNITY_EDITOR
-
-        [MenuItem("GameObject/UI/Linear Progress Bar")]
+        [MenuItem("GameObject/UI/Custom/Linear Progress Bar")]
         public static void AddLinearProgressBar()
         {
             GameObject obj = Instantiate(Resources.Load<GameObject>("LinearProgressBar"));
             obj.transform.SetParent(Selection.activeGameObject.transform,false);
         }
 
-        [MenuItem("GameObject/UI/Radial Progress Bar")]
+        [MenuItem("GameObject/UI/Custom/Radial Progress Bar")]
         public static void AddRadialProgressBar()
         {
             GameObject obj = Instantiate(Resources.Load<GameObject>("RadialProgressBar"));
             obj.transform.SetParent(Selection.activeGameObject.transform,false);
         }
-
-        #endif
     }
 }
--- Runtime/ProgressBar.cs
+++ Runtime/ProgressBar.cs
@@ -6,35 +6,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
 namespace MgSq.UI
 {
     [ExecuteInEditMode()]
     public class ProgressBar : MonoBehaviour
     {
-
-        #if UNITY_EDITOR
-
-        [MenuItem("GameObject/UI/Linear Progress Bar")]
-        public static void AddLinearProgressBar()
-        {
-            GameObject obj = Instantiate(Resources.Load<GameObject>("UI/LinearProgressBar"));
-            obj.transform.SetParent(Selection.activeGameObject.transform,false);
-        }
-
-        [MenuItem("GameObject/UI/Radial Progress Bar")]
-        public static void AddRadialProgressBar()
-        {
-            GameObject obj = Instantiate(Resources.Load<GameObject>("UI/RadialProgressBar"));
-            obj.transform.SetParent(Selection.activeGameObject.transform,false);
-        }
-
-        #endif
-
-
         [Tooltip("Weather or not there is a offest to be calculated based on a minimum value")]
         public bool DefaultBar = true;
         public int Minimum = 10;
