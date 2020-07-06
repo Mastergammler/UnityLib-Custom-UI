87ed0dbede2c4c0352731b51785e60d165b8a01e
mastergammler <raphael-hoppe@gmx.de>
2020-07-06 12:16:53 +0200

    Added editor folder and put the tooling into it

---
+++ Editor/EditorExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MgSq.UI
+{
+    public class EditorExtensions
+    {
+        #if UNITY_EDITOR
+
+        [MenuItem("GameObject/UI/Linear Progress Bar")]
+        public static void AddLinearProgressBar()
+        {
+            GameObject obj = Instantiate(Resources.Load<GameObject>("LinearProgressBar"));
+            obj.transform.SetParent(Selection.activeGameObject.transform,false);
+        }
+
+        [MenuItem("GameObject/UI/Radial Progress Bar")]
+        public static void AddRadialProgressBar()
+        {
+            GameObject obj = Instantiate(Resources.Load<GameObject>("RadialProgressBar"));
+            obj.transform.SetParent(Selection.activeGameObject.transform,false);
+        }
+
+        #endif
+    }
+}
