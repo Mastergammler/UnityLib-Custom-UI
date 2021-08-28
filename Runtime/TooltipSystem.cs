6b11c739fcaa8afdff09a664d8b45cc6870fc8b1
mastergammler <raphael-hoppe@gmx.de>
2021-08-28 11:46:26 +0200

    fixed #6 - Tooltip system doesn't access values via the static instance anymore

--- Runtime/TooltipSystem.cs
+++ Runtime/TooltipSystem.cs
@@ -1,29 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 namespace MgSq.UI
 {
+	/// <summary>
+	/// Simple Singleton to show an hide a tooltip. 
+	/// A <see cref="Tooltip"> has to set via the inspector
+	/// </summary>
 	public class TooltipSystem : MonoBehaviour
 	{
-		private static TooltipSystem mInstance;
-		public static TooltipSystem Instance => mInstance;
+		private static TooltipSystem sInstance;
+		public static TooltipSystem Instance => sInstance;
 
 		public Tooltip Tooltip;
 
 		private void Awake()
 		{
-			mInstance = this;
+			sInstance = this;
+		}
+
+		private void Start()
+		{
+			if (Tooltip == null) throw new NullReferenceException("Setup Error: A tooltip object has to be set via the inspector!");
 		}
 
 		public void Show(string content, string header = "")
 		{
-			mInstance.Tooltip.SetText(content, header);
-			mInstance.Tooltip.gameObject.SetActive(true);
+			Tooltip.SetText(content, header);
+			Tooltip.gameObject.SetActive(true);
 		}
 		public void Hide()
 		{
-			mInstance.Tooltip.gameObject.SetActive(false);
+			Tooltip.gameObject.SetActive(false);
 		}
 	}
 }
